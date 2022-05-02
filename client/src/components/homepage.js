f40b118ac30eb3866df2722223f98af9f9ab5012
heyhannibal <hannibal.inq@gmail.com>
2022-05-02 19:59:57 +0300

    added basic homepage for client

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -1,36 +1,32 @@
 import React, { useState, useEffect } from 'react'
+import uniqid from 'uniqid';
 
 
-export default  function Homepage(props) {
+export default function Homepage(props) {
 
     const [articles, setArticles] = useState(false)
-    const [isLoading, setLoading] = useState(true)
 
 
     useEffect(() => {
-        fetch('http://localhost:3000/')
+        console.log('used effect')
+        if(!articles) {fetch('http://localhost:3001/')
             .then(result => result.json())
-            .then(result => setArticles(result))
-            .then(setLoading(false))
-            console.log(articles)
-    }, [setArticles])
+            .then(result => setArticles(JSON.parse(result)))
+    }})
 
-    const list = () => {
-        if(articles) {
-            return articles.map(art => {
-                <p>{art.title}</p>
-            })
-        }
-        else return 'no articles yet'
+    function onclc () {
+        articles.forEach(item => console.log(item))
     }
-
 
     return (
         <div>
             <ul>
-                {list}
-                <button onClick={console.log(articles)}>click</button>
+               {articles ? articles.map((article) => 
+                <li key={uniqid()}><a href={article.url}>{article.title}</a></li>
+               ) :
+                'loading'}
             </ul>
         </div>
     )
 }
+
